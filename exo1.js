6adca83ab22280ab9545aea97f12a8fc662fccf0
Aymar <yannyao4066@gmail.com>
2023-01-10 13:03:02 +0000

    challenge

---
+++ exo1.js
@@ -0,0 +1,16 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+console.log(fruits)
+
+fruits.sort();
+console.log(fruits);
+
+fruits.push("Kiwi");
+console.log(fruits)
+
+fruits.splice(0,1);
+console.log(fruits)
+
+fruits.reverse()
+console.log(fruits)
